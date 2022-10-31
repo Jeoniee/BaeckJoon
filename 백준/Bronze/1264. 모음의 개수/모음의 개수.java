833696d5dcf446c335c00caba8f7859605850818
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2022-10-31 22:04:16 +0900

    [Bronze IV] Title: 모음의 개수, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		for(;;) {
+			String str = in.nextLine();
+			int count = 0;
+			if(str.equals("#")) {
+				break;
+			}
+			
+			str = str.toLowerCase();
+			
+			for(int i=0;i<str.length();i++) {
+				switch(str.charAt(i)) {
+				case 'a':
+				case 'e':
+				case 'i':
+				case 'o':
+				case 'u':
+					count++;
+				default:
+					break;
+				}
+			}
+			System.out.println(count);
+		}
+
+	}
+
+}

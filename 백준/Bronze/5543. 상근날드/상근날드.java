b97b349e787260283b911ac6589d9bf3309ce533
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2022-11-04 22:32:14 +0900

    [Bronze IV] Title: 상근날드, Time: 208 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		int burger = 2001;
+		int drink = 2001;
+		
+		//버거
+		for(int i =0;i<3;i++) {
+			int value = in.nextInt();
+			if (value < burger) { //최솟값 찾
+				burger = value;
+			}
+		}
+		
+		//드링크
+		for(int i =0;i<2;i++) {
+			int value = in.nextInt();
+			if (value < drink) { //최솟값 찾
+				drink = value;
+			}
+			
+		}
+		
+		System.out.println(burger + drink - 50);
+	}
+
+}

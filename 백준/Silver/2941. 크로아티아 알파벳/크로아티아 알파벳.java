23534904f1e82ef50b1cd888adb0991c93f0a9ce
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2023-04-12 16:44:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,22 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-
-        String[] patterns = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        for (String pattern : patterns) {
-            input = input.replaceAll(pattern, " ");
-        }
-        System.out.println(input.length());
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=", "1").length());
+	}
 }
-
-
-
-
-
